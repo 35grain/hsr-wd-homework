e098931a8779c9d2315fc5d5129632c82848cd3f
Sten Ambus <ambus.sten@gmail.com>
2021-10-22 20:36:06 +0300

    Dynamically create posts from endpoint

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+
+const displayPosts = function(data) {
+    const mainFeed = $('.feed');
+
+    for (post of data) {
+
+        const postDiv = $('<div class="userPost"></div>');
+
+        // Profile picture and username
+        const postAuthor = $('<div></div>');
+        if (post.author.profilePicture !== "")
+            postAuthor.append($('<img src="' + post.author.profilePicture + '" alt="Profile picture">'));
+        else
+            postAuthor.append($('<img src="assets/img/defaultUser.png" alt="Profile picture">'));
+        postAuthor.append($('<p>' + post.author.username + '</p>'));
+
+        const postHeader = $('<div class="userPostHeader"></div>');
+        postHeader.append(postAuthor);
+
+        // Post date
+        postHeader.append($('<p>' + post.date + '</p>'));
+        postDiv.append(postHeader);
+
+
+        // Post picture
+        const postContent = $('<div class="userPostContent"></div>');
+        if (post.cover !== "")
+            postContent.append($('<img src="' + post.cover + '" alt="Post picture">'));
+        // Post text/body
+        postContent.append($('<p>'+ post.body + '</p><button class="btn" type="button" name="like">👍</button>'));
+        postDiv.append(postContent);
+
+        mainFeed.append(postDiv);
+    }
+}
+
+$(function() {
+    $.getJSON("https://api.npoint.io/ff5e50d6586fda504c1b", function(response) {
+        displayPosts(response);
+    }).fail(function() {
+        console.log("Failed to fetch posts!");
+    })
+});
