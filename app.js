e509f2c866034b6b4a8815569a31e5639347c558
Richard A <richard.aljaste@ut.ee>
2021-12-05 15:58:14 +0200

    Added node branch for HW4

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const pool = require('./database');
+
+const app = express();
+
+// Ejs view engine
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Static files directory
+app.use(express.static('public'));
+
+// Listen on default port
+app.listen(80);
+
+app.get('/', (req, res) => {
+    res.redirect('/posts');
+});
+
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await pool.query(
+            "SELECT *, to_char(date, 'DD Mon YYYY') as date FROM posts"
+        );
+        res.render('posts', {
+            posts: posts.rows,
+            title: "Posts"
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/posts/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const posts = await pool.query(
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.render('singlepost', {
+            post: posts.rows[0],
+            title: "Post"
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.post('/posts', async (req, res) => {
+    try {
+        const post = req.body;
+        const newpost = await pool.query(
+            "INSERT INTO posts(title, body, urllink) values ($1, $2, $3) RETURNING*", [post.title, post.body, post.urllink]
+        );
+        res.redirect('posts');
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+app.get('/addnew', (req, res) => {
+    res.render('addnewpost', {
+        title: "New post"
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', {
+        title: "404"
+    });
+});
---
+++ public/assets/script.js
@@ -0,0 +1,11 @@
+$("#profile").click(function () {
+    let id = $("#dropdown")
+    switch (id.attr("class")) {
+        case "dropdown-hidden":
+            id.attr("class", "dropdown");
+            break;
+        case "dropdown":
+            id.attr("class", "dropdown-hidden");
+            break;
+    }
+});
