087ff659d7aeda714880a86af9c228eac92204f4
Richard A <richard.aljaste@ut.ee>
2021-12-12 21:57:08 +0200

    Moved like listener to script.js, modified data retrieval from HTML

--- public/assets/script.js
+++ public/assets/script.js
@@ -9,3 +9,13 @@ $("#profile").click(function () {
             break;
     }
 });
+
+$('button[name="like"]').on('click', function(e) {
+    e.preventDefault();
+    const endpoint = '/posts/' + $(this).data('like');
+    let likeCount = $('#likeCount-' + $(this).data('like'));
+    fetch(endpoint, {
+        method: 'PUT',
+    }).then(likeCount.text(parseInt(likeCount.text()) + 1),
+        $(this).prop('disabled', true));
+});
